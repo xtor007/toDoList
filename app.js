9ebecd997daa0006d2c15c5e45ba56d16fa4f528
Anatoliy <tolxpams@gmail.com>
2022-01-03 17:19:47 +0200

    simpl server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const express require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+  res.send('norm')
+})
+
+app.listen(8000,()=>{
+  console.log('norm')
+})
