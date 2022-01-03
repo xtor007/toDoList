c6a25f95543070b61d2c4f9748659d79caa2ad08
Anatoliy <tolxpams@gmail.com>
2022-01-03 16:37:21 +0200

    view

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Task from './component/task.jsx'
 
 function App() {
   return (
-    <div>hello</div>
+    <div class="site">
+      <h1>WHAT YOUR TASKS?</h1>
+      <div class="addTask">
+        <input placeholder="Write here ..."/>
+        <button>Add</button>
+      </div>
+      <Task />
+    </div>
   );
 }
 
---
+++ client/src/component/task.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let Task = (props) => {
+  return (
+    <div class="task">
+      <p>task</p>
+      <div>
+        <button>&#128396;</button>
+        <button>&#9940;</button>
+      </div>
+    </div>
+  )
+}
+
+export default Task;
