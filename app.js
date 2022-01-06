1950268e96153d618cc7f3cb9cd06784e52378c6
Anatoliy <tolxpams@gmail.com>
2022-01-07 00:51:54 +0200

    from mysql to hasura

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
 const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const schema = require('./schema.js');
 
 const app = express();
 app.use(cors());
@@ -22,43 +24,68 @@ db.connect(function(err){
   }
 });
 
+const root = {
+  getAll: () => {
+    let notr = [{id:1,name:"fdvsd",isDone:1}]
+    // db.query('SELECT * FROM tasks',function(err,result,fields){
+    //   console.log(result)
+    //   var string=JSON.stringify(result);
+    //   console.log(string)
+    //   console.log(JSON.parse(string))
+    //   return JSON.parse(string);
+    // });
+    return notr
+  },
+  createTasks: ({input}) => {
+
+  }
+}
+
+app.use('/graphql',graphqlHTTP({
+  graphiql: true,
+  schema,
+  rootValue: root
+}))
+
+
+//
 app.get('/',(req,res) => {
   db.query('SELECT * FROM tasks',function(err,result,fields){
     res.send(result)
   });
 });
-
-app.post('/',(req,res) => {
-  let data = [req.body.id,req.body.name,req.body.isDone];
-  db.query('INSERT INTO tasks (id,name,isDone) VALUES (?,?,?)',data,(err,result,fields) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-app.post('/del',(req,res) => {
-  db.query('DELETE FROM tasks WHERE id=?',req.body.id,(err,result,fields) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-app.post('/upd',(req,res) => {
-  let data = [req.body.isDone,req.body.id];
-  db.query('UPDATE tasks SET isDone = ? WHERE id = ?',data,(err,result,fields) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
+//
+// app.post('/',(req,res) => {
+//   let data = [req.body.id,req.body.name,req.body.isDone];
+//   db.query('INSERT INTO tasks (id,name,isDone) VALUES (?,?,?)',data,(err,result,fields) => {
+//     if (err) {
+//       res.json(err);
+//     } else {
+//       res.json(result);
+//     }
+//   });
+// });
+//
+// app.post('/del',(req,res) => {
+//   db.query('DELETE FROM tasks WHERE id=?',req.body.id,(err,result,fields) => {
+//     if (err) {
+//       res.json(err);
+//     } else {
+//       res.json(result);
+//     }
+//   });
+// });
+//
+// app.post('/upd',(req,res) => {
+//   let data = [req.body.isDone,req.body.id];
+//   db.query('UPDATE tasks SET isDone = ? WHERE id = ?',data,(err,result,fields) => {
+//     if (err) {
+//       res.json(err);
+//     } else {
+//       res.json(result);
+//     }
+//   });
+// });
 
 app.listen(8000,()=>{
   console.log('norm')
--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,38 @@ import axios from 'axios'
 import React from 'react'
 import Task from './component/task.jsx'
 
+function queryFetch(inQuery, inVariables) {
+  return fetch('https://new-pig-96.hasura.app/v1/graphql',{
+    method: 'POST',
+    headers: { "content-type": "aplication/json",
+               "x-hasura-admin-secret": "eRltODkyV4VCMHxjGoRnnARLNfG6oWviDAL6lH8EZnCwbmRHnTprwdXD3AQlalhQ"},
+    body: JSON.stringify({
+      query: inQuery,
+      variables: inVariables
+    })
+  }).then(res => res.json())
+}
+
 function App() {
 
   let [list,setList] = React.useState([]);
+  //let realList = null
 
   React.useEffect(() => {
-    let data = axios.get('http://localhost:8000/')
-    data.then(res => {
-      setList(res.data);
+    // let data = axios.get('http://localhost:8000/')
+    // data.then(res => {
+    //   setList(res.data);
+    // })
+    queryFetch(
+      `query getAll {
+        tasks(order_by: {date: asc}) {
+          id
+          isDone
+          name
+        }
+      }`
+    ).then(data => {
+      setList(data.data.tasks)
     })
   },[]);
 
@@ -17,21 +41,41 @@ function App() {
 
   let pushData = () => {
     if (inName.current.value != "") {
-      let post = axios.post('http://localhost:8000/',{
-        id: null,
-        name: inName.current.value,
-        isDone: 0
-      })
-      window.location.reload()
+      // let post = axios.post('http://localhost:8000/',{
+      //   id: null,
+      //   name: inName.current.value,
+      //   isDone: 0
+      // })
+      queryFetch(
+        `mutation insertData($inText: String){
+          insert_tasks_one(object: {isDone: 0, name: $inText}) {
+            id
+          }
+        }`,
+        {inText: inName.current.value}
+      )
+      //window.location.reload()
     }
   }
 
   let changeDone = (id,isDone) => {
-    let post = axios.post('http://localhost:8000/upd',{
-      id: id,
-      isDone: !isDone
-    })
-    window.location.reload()
+    // let post = axios.post('http://localhost:8000/upd',{
+    //   id: id,
+    //   isDone: !isDone
+    // })
+    // window.location.reload()
+    let newData = (isDone == 1) ? 0 : 1
+    queryFetch(
+      `mutation editIsDone($findId: uuid!, $newIsDone: Int) {
+        update_tasks_by_pk(pk_columns: {id: $findId}, _set: {isDone: $newIsDone}) {
+          id
+        }
+      }`,
+      {
+        findId: id,
+        newIsDone: newData
+      }
+    )
   }
 
   let isChecked = (isDone) => {
@@ -62,3 +106,4 @@ function App() {
 }
 
 export default App;
+export {queryFetch};
