eda125b69b0d0e2260dc0fe233107f0ee3e159c6
Anatoliy <tolxpams@gmail.com>
2022-01-07 18:58:18 +0200

    simple server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:npm run start --prefix client
+web:node server.js
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "client/build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
